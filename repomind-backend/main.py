8dd585f8ad981508a9550cd73f8d817ff67d181b
Hasaan Hameed <hasaanhameed52@gmail.com>
2026-04-10 11:14:23 +0500

    Addition: CORS Middleware

--- repomind-backend/main.py
+++ repomind-backend/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, CORSMiddleware
 from app.routes import chat, embedding
-app = FastAPI()
+
+
+app = FastAPI(title="RepoMind Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(chat.router)
